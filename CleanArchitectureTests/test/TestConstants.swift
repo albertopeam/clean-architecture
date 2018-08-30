3cc79685f4b486d078a2354f7fcac1b53de29859
Alberto Penas Amor <albertopeam@gmail.com>
2018-08-30 13:57:02 +0200

    fix...

--- CleanArchitectureTests/test/TestConstants.swift
+++ CleanArchitectureTests/test/TestConstants.swift
@@ -5,7 +5,8 @@
 //  Created by Alberto on 30/8/18.
 //  Copyright © 2018 Alberto. All rights reserved.
 //
+import Foundation
 
 class TestConstants {
-    static let timeout = 2
+    static let timeout:TimeInterval = 2
 }
